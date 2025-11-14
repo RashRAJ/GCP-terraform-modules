b466fd13444e563b5fe422587491a8f5a1e578d6
Rasheedat Atinuke Jamiu <Hartinuke16@gmail.com>
2025-11-14 12:49:06 +0100

    updates

--- compute/gke/output.tf
+++ compute/gke/output.tf
@@ -16,5 +16,9 @@ output "endpoint_ip" {
 output "token" {
   value       = google_container_cluster.primary.master_auth[0].token
   description = "Kubernetes authentication token"
-  
+}
+
+output "cluster_ca_certificate" {
+  value       = google_container_cluster.primary.master_auth[0].cluster_ca_certificate
+  description = "Kubernetes cluster CA certificate"
 }
